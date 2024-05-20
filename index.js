e6eb2dfb23a051002bc2b0f2741027c573486236
satyasanatanbastia <satyasanatanbastia@gmail.com>
2024-05-20 14:49:24 +0530

    New Commit

---
+++ index.js
@@ -0,0 +1,31 @@
+// console.log("jay sri ram");
+// console.log("jay sri radha");
+// console.log("jay Hanuman");
+// console.log("radhe radhe");
+// //data type in JS
+// let a=33
+// console.log(a);
+// var x= 22
+// console.log(x);
+// let w=55
+// let c=66
+// let z=w+c;
+// console.log(z);
+
+// //creating Object using JS
+// let Satya={
+//     firstName:"Satya Sanatan Bastia",
+//     age:22,
+//     colour:"Black"
+// }
+// // Access Object using JS
+// console.log(Satya.colour);
+// console.log(Satya.age)
+// console.log(Satya);
+
+// //Arrays in js
+// let satya=['satya sanatan bastia', 22, 'black', 33.3, true]
+// console.log(satya);
+// console.log(typeof(satya))
+// console.log(satya[3],satya[2],satya[0])
+
